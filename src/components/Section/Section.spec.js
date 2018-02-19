e79ff88954d8ed0aae04d68d89275ecd2926f572
Paul Hayes <water7earth@gmail.com>
2018-02-19 13:20:55 +0100

    recursive prototype working

--- src/components/Section/Section.spec.js
+++ src/components/Section/Section.spec.js
@@ -2,15 +2,7 @@ import React from 'react';
 import toJson from 'enzyme-to-json';
 import { shallow } from 'enzyme';
 import Section from './Section';
-
-const mockSettings = {
-  id: 'id',
-  className: 'a-class another-class',
-  style: {
-    backgroundColor: 'red',
-    fontSize: 16,
-  }
-}
+import { mockSettings } from '../../utils/testHelpers';
 
 test('Section component should render as expected', () => {
   const component = shallow(<Section settings={mockSettings} children={null} />);
---
+++ src/utils/testHelpers.js
@@ -0,0 +1,10 @@
+export const mockSettings = {
+  id: 'id',
+  className: 'a-class another-class',
+  style: {
+    backgroundColor: 'red',
+    fontSize: 16,
+  }
+}
+
+export const mockHtml = '<div>Hello test world</div>';
